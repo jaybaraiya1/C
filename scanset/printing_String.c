2631e1600a713c0200d1ae85e43337c3a5630d98
jay baraiya <140989637+jaybaraiya1@users.noreply.github.com>
2025-06-19 21:20:14 +0530

    Add files via upload

---
+++ scanset/printing_String.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+#include<string.h>
+void main(){
+    char str[10];
+    gets(str);
+    puts(str);
+}
---
+++ scanset/use_of_xor.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+#include<string.h>
+void main(){
+    char *str;
+    scanf("%[^(a-z)]s",str);
+    printf("%s",str);
+}
